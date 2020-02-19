f6d2d3e70ba73a13177729ac52bd98ee62e8e47e
MdoubleA <32501929+MdoubleA@users.noreply.github.com>
2020-02-19 02:42:39 -0800

    Find number of words shared and not shared between the 3 sets.

--- FrequencyDistro_Jennie/FreqDistroAnalysis.py
+++ FrequencyDistro_Jennie/FreqDistroAnalysis.py
@@ -4,14 +4,39 @@ import nltk
 import shutil
 import csv
 from nltk.corpus import stopwords
+from enum import Enum
 
 
-the_file = 'most_freq_words.csv'
+all_files = [
+    'most_freq_words.csv',
+    'most_freq_words_non_sarc.csv',
+    'most_freq_words_sarc.csv',
+]
 
-with open(the_file, newline='') as the_csv_file:
-    the_csv_file = list(csv.reader(the_csv_file, delimiter=','))[1:]  # Remove header.
-    raw_count = sum([1 for row in the_csv_file])
-    stop_words = set(stopwords.words('english'))
-    stop_word_count = sum([1 for row in the_csv_file if row[0] not in stop_words])
-    print("Raw count: " + str(raw_count))
-    print("stop word count: " + str(stop_word_count))
+both = 0
+nonsarcastic = 1
+sarcastic = 2
+
+
+def get_csv(the_file):
+    with open(the_file, newline='') as the_csv_file:
+        the_csv_file = list(csv.reader(the_csv_file, delimiter=','))[1:]  # Remove header.
+    return the_csv_file
+
+
+all_words = [word[0] for word in get_csv(all_files[both])]
+nonsarcastic_words = [word[0] for word in get_csv(all_files[nonsarcastic])]
+sarcastic_words = [word[0] for word in get_csv(all_files[sarcastic])]
+
+common_words = [x for x in nonsarcastic_words if x in sarcastic_words]
+not_common_words = [x for x in nonsarcastic_words if x not in sarcastic_words]
+
+print("Total number of words: " + str(len(all_words)))
+print("Number of words shared between subsets: " + str(len(common_words)))
+print("Number of words not shared between subsets: " + str(len(not_common_words)))
+print("\n")
+print("Number of words shared between set and nonsarcastic set: " + str(len([x for x in all_words if x in nonsarcastic_words])))
+print("Number of words not shared between set and nonsarcastic set: " + str(len([x for x in all_words if x not in nonsarcastic_words])))
+print("\n")
+print("Number of words shared between set and sarcastic set: " + str(len([x for x in all_words if x in sarcastic_words])))
+print("Number of words not shared between set and sarcastic set: " + str(len([x for x in all_words if x not in sarcastic_words])))
