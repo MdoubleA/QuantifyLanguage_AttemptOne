46e4b7c969b001f9fbf4280c5683a5654ef26c40
MdoubleA <32501929+MdoubleA@users.noreply.github.com>
2020-02-19 03:32:40 -0800

    Find words shared and not shared between sets.

--- FrequencyDistro_Jennie/FreqDistroAnalysis.py
+++ FrequencyDistro_Jennie/FreqDistroAnalysis.py
@@ -25,15 +25,52 @@ num_non = len(nonsarcastic_words)
 sarcastic_words = [word[0] for word in get_csv(all_files[sarcastic])]
 num_sarc = len(sarcastic_words)
 
-common_words = [x for x in nonsarcastic_words if x in sarcastic_words]
-not_common_words = [x for x in nonsarcastic_words if x not in sarcastic_words]
+words_shared = lambda x, y: [z for z in x if z in y]
+words_not_shared = lambda x, y: [z for z in x if z not in y]
 
 print("Number of words in set: " + str(len(all_words)))
-print("Number of words shared between subsets: " + str(len(common_words)))
-print("Number of words not shared between subsets: " + str(len(not_common_words)))
+print("Number of words shared between subsets: " + str(len(words_shared(sarcastic_words, nonsarcastic_words))))
+print("Number of words not shared between subsets: " + str(len((words_not_shared(sarcastic_words, nonsarcastic_words)))))
 print("\n")
-print("Number of words shared between set and nonsarcastic set: " + str(len([x for x in all_words if x in nonsarcastic_words])))
-print("Number of words not shared between set and nonsarcastic set: " + str(len([x for x in all_words if x not in nonsarcastic_words])))
+print("Number of words shared between set and nonsarcastic set: " + str(len(words_shared(all_words, nonsarcastic_words))))
+print("Number of words not shared between set and nonsarcastic set: " + str(len(words_not_shared(all_words, nonsarcastic_words))))
 print("\n")
-print("Number of words shared between set and sarcastic set: " + str(len([x for x in all_words if x in sarcastic_words])))
-print("Number of words not shared between set and sarcastic set: " + str(len([x for x in all_words if x not in sarcastic_words])))
+print("Number of words shared between set and sarcastic set: " + str(len(words_shared(all_words, sarcastic_words))))
+print("Number of words not shared between set and sarcastic set: " + str(len(words_not_shared(all_words, sarcastic_words))))
+print("\n")
+
+def print_shared_words(a, b):
+    max_line = 15
+    temp = words_shared(a, b)
+    for x in [temp[y:y+max_line] for y in range(0, len(temp), max_line)]:
+        print(', '.join(x))
+
+def print_not_shared_words(a, b):
+    max_line = 15
+    temp = words_not_shared(a, b)
+    for x in [temp[y:y+max_line] for y in range(0, len(temp), max_line)]:
+        print(', '.join(x))
+
+print("The words shared between subsets: ")
+print_shared_words(nonsarcastic_words, sarcastic_words)
+print("\n")
+
+print("The words not shared between subsets: ")
+print_not_shared_words(nonsarcastic_words, sarcastic_words)
+print("\n")
+
+print("The words shared between set and nonsarcastic set: ")
+print_shared_words(all_words, nonsarcastic_words)
+print("\n")
+
+print("The words not shared between set and nonsarcastic set: ")
+print_not_shared_words(all_words, nonsarcastic_words)
+print("\n")
+
+print("The words shared between set and sarcastic set: ")
+print_shared_words(all_words, sarcastic_words)
+print("\n")
+
+print("The words not shared between set and sarcastic set: ")
+print_not_shared_words(all_words, sarcastic_words)
+print("\n")
