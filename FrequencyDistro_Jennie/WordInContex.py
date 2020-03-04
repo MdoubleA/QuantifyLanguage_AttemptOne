f2bc14f5f00a11e03a6b974575996a2cbed681fc
MdoubleA <32501929+MdoubleA@users.noreply.github.com>
2020-03-03 22:16:03 -0800

    Fine tune understanding of what statistics to get.

--- FrequencyDistro_Jennie/WordInContex.py
+++ FrequencyDistro_Jennie/WordInContex.py
@@ -1,4 +1,6 @@
 import nltk
+from collections import Counter
+import functools
 
 
 REF = "Reference"
@@ -13,6 +15,7 @@ src_file = "UniqueWordsAndSharedWords.txt"
 
 
 # Get the most common unique words by type.
+# NEED to get the most common word WITH its context. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 def get_unique_words(file=src_file):
     to_ret = dict()
     with open(file, "r") as file_handle:
@@ -37,26 +40,66 @@ sarc_file_name = "C:\\Users\\Michael\\PycharmProjects\\QuantfyLanguage_AttemptOn
 non_sarc_file_name = "C:\\Users\\Michael\\PycharmProjects\\QuantfyLanguage_AttemptOne\\NonSarcCommentsClean\\" \
                      "NonSarcComments.csv"
 
+#  Get clean tokenized data.
 sarc_text = get_corpus(sarc_file_name)
 non_sarc_text = get_corpus(non_sarc_file_name)
 all_words_text = nltk.Text(list(sarc_text) + list(non_sarc_text))
 unique_words = get_unique_words()  # {str : [str]}
+
+# Map tokenized data to type shorthands.
 type_to_corpus = {
     REF: all_words_text,
     NON: non_sarc_text,
-    SAR: sarc_text
+    SAR: sarc_text,
+    SHA: all_words_text,
 }
 
-for key in type_to_number.keys():
-    if key != SHA:
+
+# Find the context of each key word in each type of set and store the context to file.
+# Temporarily searching right context, only.
+# Don't NEED to get this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def get_key_word_context_to_file():
+    for key in type_to_number.keys():
         with open(key + ".txt", "w") as file_handle:
             for token in unique_words[key]:
                 file_handle.write("*" + token + "\n")
-                contex = type_to_corpus[key].concordance_list(token, width=15, lines=500)
-                for a in contex:
+                context = type_to_corpus[key].concordance_list(token, width=15, lines=500)
+                for a in context:
                     #file_handle.write(", ".join(a.left) + "\n")
                     file_handle.write(", ".join(a.right) + "\n")
 
+
+#  get_key_word_context_to_file()
+
+
+# NEED to get probabilities for each phrase
+def get_context_frequency_distro():
+    for classification in type_to_number.keys():
+        with open(classification + ".txt", "r") as file_handle:
+            key_word = file_handle.readline().strip()[1:]
+            line = file_handle.readline()
+            context = list()
+            while line[0] != "*":
+                context += line.strip().split(", ")
+                line = file_handle.readline()
+
+        # Make frequency distribution.
+        word_counts = Counter(context)
+        tot_num_word = len(context)
+        freq_distro = {key: (value/tot_num_word) * 100 for key, value in word_counts.items()}
+        freq_distro = [(k, v) for k, v in sorted(freq_distro.items(), key=lambda item: item[1])]
+        freq_distro.reverse()
+
+        with open(classification + "Context" + "Freq" + ".txt", "w") as file_handle:
+            file_handle.write("Total Word Count:" + str(tot_num_word) + "\n")
+            for data_point in freq_distro:
+                file_handle.write(data_point[0] + " " + str(data_point[1]) + "\n")
+
+
+#get_context_frequency_distro()
+
+'''
+#  Get the Context of specific type of data from file.
 with open("Sarcastic.txt", "r") as file_handle:
     key_word = file_handle.readline().strip()[1:]
     line = file_handle.readline()
@@ -65,7 +108,27 @@ with open("Sarcastic.txt", "r") as file_handle:
         context += line.strip().split(", ")
         line = file_handle.readline()
 
-result = sorted(context, key=context.count, reverse=True)
-print(result)
+#result = sorted(context, key=context.count, reverse=True)
+#print(result)
 
+# Make frequency distribution.
+word_counts = Counter(context)
+print(word_counts)
+tot_num_word = len(context)
+print(len(context))
+freq_distro = {key: (value/tot_num_word) * 100 for key, value in word_counts.items()}
+freq_distro = [(k, v) for k, v in sorted(freq_distro.items(), key=lambda item: item[1])]
+freq_distro.reverse()
+print(freq_distro)
+'''
 
+''' Testing that the keys in word_count and freq_distro remain constant across processing 
+# functools.reduce(function, iterable[, initializer])
+x = [True for y in freq_distro if y[0] in word_counts.keys()]
+x = functools.reduce(lambda a, b: a and b, x)
+r = [y[0] for y in freq_distro]
+print(x)
+x = [True for y in word_counts.keys() if y in r]
+x = functools.reduce(lambda a, b: a and b, x)
+print(x)
+'''
